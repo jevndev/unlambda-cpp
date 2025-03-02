9f557e37082ce3cc3f69a4f43e298cd4c332c411
Jonathan Evans <JonathanEvansDev@gmail.com>
2025-03-02 14:14:41 -0600

    Loosen IO constraints of program

--- src/unlambda.hpp
+++ src/unlambda.hpp
@@ -8,19 +8,35 @@ namespace Unlambda {
 
 template <typename T>
 concept ProgramOutput = requires(T &t, char c) {
-  { t.operator<<(c) } -> std::same_as<T &>;
+  { t.operator<<(c) };
 };
 
 template <typename T>
 concept ProgramInput = requires(T &t, int &c) {
-  { t.operator>>(c) } -> std::same_as<T &>;
+  { t.operator>>(c) };
 };
+
+class NoOutput_t {
+public:
+  constexpr NoOutput_t &operator<<(const char c) { return *this; };
+};
+
+static auto noOutput = NoOutput_t{};
+
+class NoInput_t {
+public:
+  constexpr NoInput_t &operator>>(int &c) { return *this; };
+};
+
+static auto noInput = NoInput_t{};
 
 class Program {
 public:
   constexpr explicit Program(const std::string_view source) noexcept {}
   constexpr void evaluate(ProgramOutput auto &output,
                           ProgramInput auto &input) noexcept {}
+
+  constexpr void evaluate() noexcept { evaluate(noOutput, noInput); }
 };
 
 constexpr Program operator""_unl(const char *source, const size_t n) {
